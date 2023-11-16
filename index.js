0dd1316ac2ae691f602d35f3c16bddb6f73d61a0
Andrus Rinde <andrusrinde@users.noreply.github.com>
2023-11-16 13:07:05 +0200

    10 tund

--- index.js
+++ index.js
@@ -5,14 +5,28 @@ const mysql = require('mysql2');
 const bodyparser = require('body-parser');
 const dateInfo = require('./dateTimeFnc');
 const dbConfig = require('../../../vp23config');
-const dataBase = 'if23_inga_pe_DM';
+const dBase = 'if23_inga_pe_DM';
+const dataBase = 'if23_rinde';
+const multer = require('multer');
+//seame multer jaoks vahevara, mis määrab üleslaadimise kataloogi
+const upload = multer({dest: './public/gallery/orig/'});
+const mime = require('mime');
+const sharp = require('sharp');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(bodyparser.urlencoded({extended: false}));
+//app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.urlencoded({extended: true}));
 
 //loon andmebaasiühenduse
 const conn = mysql.createConnection({
+	host: dbConfig.configData.host,
+	user: dbConfig.configData.user,
+	password: dbConfig.configData.password,
+	database: dBase
+});
+
+const connection = mysql.createConnection({
 	host: dbConfig.configData.host,
 	user: dbConfig.configData.user,
 	password: dbConfig.configData.password,
@@ -93,6 +107,49 @@ app.get('/news/read/:id', (req,res)=> {
 	console.log(req.params);
 	console.log(req.query);
 	res.send('Vaatame uudist, mille id on: ' + req.params.id);
+});
+
+app.get('/photoupload', (req, res)=> {
+	res.render('photoupload');
+});
+
+app.post('/photoupload', upload.single('photoInput'), (req, res)=> {
+	let notice = '';
+	console.log(req.file);
+	console.log(req.body);
+	//const mimeType = mime.getType(req.file.path);
+	//console.log(mimeType);
+	const fileName = 'vp_' + Date.now() + '.jpg';
+	//fs.rename(req.file.path, './public/gallery/orig/' + req.file.originalname, (err)=> {
+	fs.rename(req.file.path, './public/gallery/orig/' + fileName, (err)=> {
+		console.log('Viga: ' + err);
+	});
+	const mimeType = mime.getType('./public/gallery/orig/' + fileName);
+	console.log('Tüüp: ' + mimeType);
+	//loon pildist pisipildi (thumbnail)
+	sharp('./public/gallery/orig/' + fileName).resize(800,600).jpeg({quality : 90}).toFile('./public/gallery/normal/' + fileName);
+	sharp('./public/gallery/orig/' + fileName).resize(100,100).jpeg({quality : 90}).toFile('./public/gallery/thumbs/' + fileName);
+	
+	
+	let sql = 'INSERT INTO vp_gallery (filename, originalname, alttext, privacy, userid) VALUES (?,?,?,?,?)';
+	const userid = 1;
+	connection.query(sql, [fileName, req.file.originalname, req.body.altInput, req.body.privacyInput, userid], (err, result)=>{
+		if(err) {
+			throw err;
+			notice = 'Foto andmete salvestamine ebaõnnestus!' + err;
+			res.render('photoupload', {notice: notice});
+		}
+		else {
+			notice = 'Pilt "' + req.file.originalname + '" laeti üles!';
+			res.render('photoupload', {notice: notice});
+		}
+	});
+	
+	
+});
+
+app.get('/photogallery', (req, res)=> {
+	res.render('photogallery');
 });
 
 app.post('/eestifilm/lisapersoon', (req, res)=>{
